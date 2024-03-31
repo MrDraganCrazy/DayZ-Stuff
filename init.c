7561fd2ae047ed4e33c8f3492618df67e05e1b35
MrDraganCrazy <133111877+MrDraganCrazy@users.noreply.github.com>
2024-03-31 11:27:47 +0200

    Add files via upload

---
+++ init.c
@@ -0,0 +1,106 @@
+void main()
+{
+	CreateHive();
+	GetHive().InitOffline();
+	int year, month, day, hour, minute;
+	GetGame().GetWorld().GetDate( year, month, day, hour, minute );
+
+	//Change here the dates for whatever months you desire
+    if ( month < 09 )
+    {
+    	year = 2024;
+        month = 09;
+        day = 25;
+		
+		GetGame().GetWorld().SetDate( year, month, day, hour, minute );
+	}
+} 
+
+class CustomMission: MissionServer
+
+{
+	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
+	{
+		Entity playerEnt = GetGame().CreatePlayer(identity, characterName, pos, 0, "NONE");
+		Class.CastTo(m_player, playerEnt);
+		GetGame().SelectPlayer(identity, m_player);
+
+		return m_player;
+	}
+
+	void addMags(PlayerBase player, string mag_type, int count)
+	{
+		if (count < 1)
+			return;
+
+		EntityAI mag;
+
+		for (int i = 0; i < count; i++) {
+			mag = player.GetInventory().CreateInInventory(mag_type);
+		}
+
+		player.SetQuickBarEntityShortcut(mag, 1, true);
+	}
+
+	EntityAI assaultClass(PlayerBase player)
+	{
+		EntityAI gun = player.GetHumanInventory().CreateInHands("M4A1");
+		gun.GetInventory().CreateAttachment("M4_RISHndgrd_Black");
+		gun.GetInventory().CreateAttachment("M4_MPBttstck_Black");
+		gun.GetInventory().CreateAttachment("ACOGOptic");
+		addMags(player, "Mag_STANAG_30Rnd", 3);
+
+		return gun;
+	}
+
+	EntityAI sniperClass(PlayerBase player)
+	{
+		EntityAI gun = player.GetHumanInventory().CreateInHands("SVD");
+		gun.GetInventory().CreateAttachment("PSO1Optic");
+		addMags(player, "Mag_SVD_10Rnd", 3);
+
+		return gun;
+	}
+
+	EntityAI smgClass(PlayerBase player)
+	{
+		EntityAI gun = player.GetHumanInventory().CreateInHands("UMP45");
+		gun.GetInventory().CreateAttachment("PistolSuppressor");
+		addMags(player, "Mag_UMP_25Rnd", 3);
+
+		return gun;
+	}
+
+	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	{
+		player.RemoveAllItems();
+		
+		player.GetInventory().CreateInInventory("Crazy_Prisoner_Shirt");
+		player.GetInventory().CreateInInventory("Crazy_Prisoner_Pants");
+		player.GetInventory().CreateInInventory("Crazy_Shoes_Prisoner");
+		player.GetInventory().CreateInInventory("Crazy_Radler");
+		player.GetInventory().CreateInInventory("Crazy_Sarma");
+		player.GetInventory().CreateInInventory("HuntingKnife");
+		player.GetInventory().CreateInInventory("BandageDressing");
+		ItemBase rags = player.GetInventory().CreateInInventory("Rag");
+		rags.SetQuantity(4);
+
+		EntityAI primary;
+		EntityAI axe = player.GetInventory().CreateInInventory("");
+
+		switch (Math.RandomInt(0, 3)) {
+			case 0: primary = assaultClass(player); break;
+			case 1: primary = sniperClass(player); break;
+			case 2: primary = smgClass(player); break;
+		}
+
+		player.LocalTakeEntityToHands(primary);
+		player.SetQuickBarEntityShortcut(primary, 0, true);
+		player.SetQuickBarEntityShortcut(rags, 2, true);
+	}
+};
+
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
+}
